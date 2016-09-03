3ff3570f07663677a1b32b36444865d7078831f6
Gabriel García Seco <ggarciaseco@gmail.com>
2016-09-03 10:34:59 +0200

    Update sails controller

---
+++ api/controllers/RepeatedWordsController.js
@@ -0,0 +1,11 @@
+/**
+ * RepeatedWordsController
+ *
+ * @description :: Server-side logic for managing Repeatedwords
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
